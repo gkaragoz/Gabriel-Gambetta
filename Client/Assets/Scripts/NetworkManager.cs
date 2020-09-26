0167a5b84c68c947a0672321c76b528f2bebf6e0
gkaragoz <gkaragoz96@gmail.com>
2020-09-27 00:55:55 +0300

    Refactor clientside

--- Client/Assets/Scripts/NetworkManager.cs
+++ Client/Assets/Scripts/NetworkManager.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using BestHTTP;
+﻿using UnityEngine;
 using BestHTTP.SocketIO;
 using System;
 
 public class NetworkManager : MonoBehaviour {
+
+    public static NetworkManager instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(instance);
+    }
 
     [SerializeField]
     private string URI = "http://localhost";
@@ -21,7 +30,19 @@ public class NetworkManager : MonoBehaviour {
         _socketManager.Socket.On("connect", OnConnected);
         _socketManager.Socket.On("welcome_message", OnWelcomeMessageReceived);
         _socketManager.Socket.On("disconnect", OnDisconnected);
+
+        _socketManager.Socket.On("init_players", OnInitPlayersReceived);
+        _socketManager.Socket.On("broadcast_positions", OnBroadcastPositionsReceived);
     }
+
+    private void Update() {
+        if (_socketManager.Socket.IsOpen) {
+            float posX = transform.position.x;
+            _socketManager.Socket.Emit("get_position", posX);
+        }
+    }
+
+    #region DEFAULT CONNECTIONS/DISCONNECTION
 
     private void OnConnecting(Socket socket, Packet packet, object[] args) {
         Debug.Log("OnConnecting... to " + _socketManager.Uri);
@@ -32,10 +53,25 @@ public class NetworkManager : MonoBehaviour {
     }
 
     private void OnWelcomeMessageReceived(Socket socket, Packet packet, object[] args) {
-        Debug.Log("Receive OnWelcomeMessage");
+        Debug.Log("Receive OnWelcomeMessage " + packet.Payload);
     }
 
     private void OnDisconnected(Socket socket, Packet packet, object[] args) {
         Debug.Log("OnDisconnect");
     }
+
+    #endregion
+
+    #region CUSTOM EVENTS
+
+    private void OnInitPlayersReceived(Socket socket, Packet packet, object[] args) {
+        Debug.Log("OnInitPlayersReceived " + packet.Payload);
+    }
+
+    private void OnBroadcastPositionsReceived(Socket socket, Packet packet, object[] args) {
+        Debug.Log("OnBroadcastPositionsReceived " + packet.Payload);
+    }
+
+    #endregion
+
 }
